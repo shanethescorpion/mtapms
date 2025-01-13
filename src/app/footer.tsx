0ba3eea8af466803b2bd1ee58758257f36533af7
shanethescorpion <jae04093@gmail.com>
2025-01-13 10:23:50 +0800

    Update footer.tsx

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <div className="text-center font-[600] text-[#6C6C6C] text-[11px] leading-[13.41px]">
-      <p><span className="text-[#468966]">LGU Website</span> | Developed by: | Erika Shane Labor | Reginald S. Laspiñas | Regelou Wenceslao</p>
+      <p><span className="text-[#468966]">MTAPMS</span> | Developed by: | Erika Shane Labor | Reginald S. Laspiñas | Regelou Wenceslao</p>
       <p>College  of Computing and Information Sciences (Saint Michael College of Caraga)</p>
     </div>
   )
